aac372a9e02407720d575f15035f8a0b1e51adea
Williamm15 <93446321+Williamm15@users.noreply.github.com>
2021-10-31 13:43:41 -0500

    Add files via upload

--- WilliamsonCSC422Assignment1.java
+++ WilliamsonCSC422Assignment1.java
@@ -10,7 +10,6 @@ package williamsoncsc422assignment1;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
 public class WilliamsonCSC422Assignment1 {
 
 // As Pet Objects are created, they are added to this ArrayList
@@ -19,27 +18,42 @@ public static ArrayList<Pet> petList = new ArrayList<>();
     public static void main(String[] args) {
         // validator is what controls the while loop. While true, the program runs. If it is false, the program is closed
         boolean validator =  true;
-        
         Scanner sc = new Scanner(System.in);
         do {
             // calling the menuDisplay method
             menuDisplay();
-            int input = sc.nextInt();
-            // Switch controls output based on what option is typed by the user
-            switch(input){
-                case 1: 
-                    // calling the petDisplay method
-                    petDisplay();
-                    break;
-                case 2:
-                    // calling the addPet method
-                    addPet();
-                    break;
-                case 3:
-                    // switching the validator boolean from true to false. This breaks the while loop and closes to program
-                    System.out.println("Goodbye!");
-                    validator = false;
-                    break;
+            String strInput = sc.nextLine();
+            // checking is the input is an int
+            boolean inputCheck = isInt(strInput);
+            if (inputCheck){
+                int input = Integer.parseInt(strInput);
+                // Switch controls output based on what option is typed by the user
+                switch(input){
+                    case 1: 
+                        // calling the petDisplay method
+                        petDisplay();
+                        break;
+                    case 2:
+                        // calling the addPet method
+                        addPet();
+                        break;
+                    case 3:
+                        // calling searchName method
+                        searchName();
+                        break;
+                    case 4:
+                        // calling searchAge method
+                        searchAge();
+                        break;
+                    case 5:
+                        // switching the validator boolean from true to false. This breaks the while loop and closes to program
+                        System.out.println("Goodbye!");
+                        validator = false;
+                        break;
+                }
+            }
+            else{
+                System.out.println("Error: Please type an integer as your choice.");
             }
         }while(validator);
     }
@@ -48,7 +62,9 @@ public static ArrayList<Pet> petList = new ArrayList<>();
         System.out.println("\nWhat would you like to do?");
         System.out.println("1) View all pets");
         System.out.println("2) Add more pets");
-        System.out.println("3) Exit program");
+        System.out.println("3) Search pets by name");
+        System.out.println("4) Search pets by age");
+        System.out.println("5) Exit program");
         System.out.print("Your choice: ");
     }
     // this method displays the data that is the the petList ArrayList to the user
@@ -58,7 +74,6 @@ public static ArrayList<Pet> petList = new ArrayList<>();
         System.out.printf("|%-10s", " NAME ");
         System.out.printf("|%-4s", " AGE ");
         System.out.print("|\n");
-        
         System.out.println("+---------------------+");
         for (int i = 0; i < petList.size(); i++){
             System.out.printf("|%3d" , i);
@@ -112,6 +127,74 @@ public static ArrayList<Pet> petList = new ArrayList<>();
                 else{
                     System.out.println("Error: Please enter a name and an age separated by a space.");
                 }
+            }
+        }
+    }
+    // searchName method searches pets by the name entered
+    public static void searchName(){
+        System.out.print("\nEnter a name to search: ");
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        System.out.println("\n+---------------------+");
+        System.out.printf("|%-3s", " ID ");
+        System.out.printf("|%-10s", " NAME ");
+        System.out.printf("|%-4s", " AGE ");
+        System.out.print("|\n");
+        System.out.println("+---------------------+");
+        // rows counts how many pets are printed
+        int rows = 0;
+        // looping through each index of the array
+        for(int i = 0; i < petList.size(); i++){
+            // if the name entered matches a name that is in the petList ArrayList, it is displayed
+            if (name.toLowerCase().equals(petList.get(i).getName().toLowerCase())){
+                System.out.printf("|%3d" , i);
+                System.out.print(" | ");
+                System.out.printf("%-9s" , petList.get(i).getName());
+                System.out.printf("|%4d" , petList.get(i).getAge());
+                System.out.print(" |\n");
+                rows++;
+            }
+        }
+        System.out.println("+---------------------+");
+        System.out.println(rows + " rows in set.\n");
+    }
+    // searchAge method searches pets by the age entered
+    public static void searchAge(){
+        // loop runs until break is read after input is validated as an int
+        while(true){
+            System.out.print("\nEnter an age to search: ");
+            Scanner sc = new Scanner(System.in);
+            String strAge = sc.nextLine();
+            // checking if input is an int
+            Boolean ageCheck = isInt(strAge);
+            if (ageCheck){
+                int age = Integer.parseInt(strAge);
+                System.out.println("\n+---------------------+");
+                System.out.printf("|%-3s", " ID ");
+                System.out.printf("|%-10s", " NAME ");
+                System.out.printf("|%-4s", " AGE ");
+                System.out.print("|\n");
+                System.out.println("+---------------------+");
+                // rows counts how many pets are printed
+                int rows = 0;
+                // looping through each index of the array
+                for(int i = 0; i < petList.size(); i++){
+                    // if the age entered matches an age that is in the petList ArrayList, it is displayed
+                    if (age == petList.get(i).getAge()){
+                        System.out.printf("|%3d" , i);
+                        System.out.print(" | ");
+                        System.out.printf("%-9s" , petList.get(i).getName());
+                        System.out.printf("|%4d" , petList.get(i).getAge());
+                        System.out.print(" |\n");
+                        rows++;
+                    }
+                }
+                System.out.println("+---------------------+");
+                System.out.println(rows + " rows in set.\n");
+                break;
+            }
+            else{
+                System.out.println("Error: Please type an integer as an age.");
             }
         }
     }
